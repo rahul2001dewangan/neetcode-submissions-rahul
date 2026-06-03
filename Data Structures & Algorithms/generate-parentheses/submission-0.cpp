50e79a40bd581a24457004d91f862cb4b55599d5
Rahul Dewangan <96685874+rahul2001dewangan@users.noreply.github.com>
2026-06-03 14:43:46 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void parantheses(int& n, int o, int c, string s, vector<string>& ans) {
+        if (o == n && c == n) {
+            ans.push_back(s);
+            return;
+        }
+        if (o < n) {
+            parantheses(n, o + 1, c, s + '(', ans);
+        }
+        if (c < o) {
+            parantheses(n, o, c + 1, s + ')', ans);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string s = "";
+        parantheses(n, 0, 0, s, ans);
+        return ans;
+    }
+};
