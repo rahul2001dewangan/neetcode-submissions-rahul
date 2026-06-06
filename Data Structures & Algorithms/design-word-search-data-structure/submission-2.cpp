92c0e82423498b4e1361895cb9cd58c41a97804a
Rahul Dewangan <96685874+rahul2001dewangan@users.noreply.github.com>
2026-06-06 14:17:32 +0530

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.cpp
@@ -0,0 +1,50 @@
+class WordDictionary {
+public:
+    bool terminal;
+    WordDictionary* children[26];
+    WordDictionary() {
+        terminal = false;
+        for (int i = 0; i < 26; i++) {
+            children[i] = NULL;
+        }
+    }
+
+    void addWord(string word) {
+        WordDictionary* node = this;
+        for (char c : word) {
+            int idx = c - 'a';
+            if (!node->children[idx]) {
+                node->children[idx] = new WordDictionary;
+            }
+            node = node->children[idx];
+        }
+        node->terminal = true;
+    }
+
+    bool searchWord(string& word, int i, WordDictionary* root) {
+        if (!root) {
+            return false;
+        }
+        if (i == word.size()) {
+            return root->terminal;
+        }
+        if (word[i] != '.') {
+            return searchWord(word, i + 1, root->children[word[i] - 'a']);
+        }
+        for (int j = 0; j < 26; j++) {
+            if (searchWord(word, i + 1, root->children[j])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool search(string word) { return searchWord(word, 0, this); }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
